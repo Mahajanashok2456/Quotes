5dcc4cdabf280d10616bbcd5eaf6560048881faa
mahajanashok2456 <ashokroshan78@gmail.com>
2025-10-20 02:00:40 +0530

    Fix middleware to only protect secret admin route and prevent redirect loop

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/manage-mahajan/:path*', '/api/admin/:path*'],
+  matcher: '/manage-content-a3f8b1c9/:path*',
 };
