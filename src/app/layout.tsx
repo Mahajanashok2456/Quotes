decf903d002ce5406ca8a25cb309ef93e6436ae2
mahajanashok2456 <ashokroshan78@gmail.com>
2025-10-23 23:36:23 +0530

    Update AdSense script strategy from beforeInteractive to lazyOnload for better verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9258915549707323" 
           crossOrigin="anonymous" 
-          strategy="beforeInteractive" 
+          strategy="lazyOnload" 
         />
       </head>
       <body
