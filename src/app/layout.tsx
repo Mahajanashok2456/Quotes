bca94079539de88b5553d34534454e5b58ae1f93
mahajanashok2456 <ashokroshan78@gmail.com>
2025-11-04 12:57:11 +0530

    Add Google Tag Manager (GTM-NLSLPLH4) to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,22 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NLSLPLH4');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="icon"
@@ -90,6 +106,17 @@ export default function RootLayout({
         suppressHydrationWarning
         style={{ margin: 0, padding: 0 }}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NLSLPLH4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header />
         <div className="overlay"></div>
         <Providers>{children}</Providers>
